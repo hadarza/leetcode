ff720371f8a0a5081a1198774bba0973133836a7
hadar <hadar.zaguri@gmail.com>
2023-05-23 23:35:36 +0300

    start ex15

---
+++ medium/ex15.js
@@ -0,0 +1,13 @@
+/*Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0. */
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    //1 2 4 5 6 7 8 9
+    let indexA = 0,indexB=nums.length-1;
+    let pointerA = nums[indexA]
+    let PointerB = nums[indexB]
+  
+};
